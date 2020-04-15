88b71b19a7863a4132492c64adec0843475158c7
ceecyl.lang@gmail.com <ceecyl.lang@gmail.com>
2020-04-15 15:42:10 +0200

    add(accueil) add function Accueil

---
+++ src/components/accueil.jsx
@@ -0,0 +1,15 @@
+
+import React from 'react';
+
+
+function Accueil() {
+  return (
+    <div>
+      <img src="https://zupimages.net/up/20/16/gwq6.png" alt="Logo Hero Fate" />
+      <p>Slogan</p>
+      <button type="button">Play</button>
+    </div>
+  );
+}
+
+export default Accueil();
