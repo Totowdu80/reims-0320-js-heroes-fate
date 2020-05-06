f6c938398a85a939c4361927d7f57b0455c10245
ceecyl.lang@gmail.com <ceecyl.lang@gmail.com>
2020-05-06 10:05:42 +0200

    connect(instruction) connect style in instruction

--- src/components/Homepage/Instructions/instruction.jsx
+++ src/components/Homepage/Instructions/instruction.jsx
@@ -4,7 +4,7 @@ import './instruction.css';
 function Instruction() {
   return (
     <div>
-      <h1> Bienvenue dans Heroes Fate,</h1>
+      <h1 className="titleInstruction"> Bienvenue dans Heroes Fate,</h1>
       <p>
         C'est un jeu de combat entre un héros et une vague de super méchant,
         Une fois votre Univers choisi, vous aurez le choix entre plusieurs 
@@ -14,12 +14,12 @@ function Instruction() {
         après c'est a vous d'attaquer et a lui de ce défendre, a tour jusqu'à ce que l'un des deux n'est plus de point de vie
       </p>
       <p>
-        Pour la phases d'attaque vous avez le choix entre trois type attaque différente,(nom des attaques) qui font plus ou moins de dégâts de la plus forte a la moins forte
+        Pour la phases d'attaque vous avez le choix entre trois type attaque différente, qui font plus ou moins de dégâts de la plus forte a la moins forte
         sachant que si vous choisissez l'attaque la plus forte vous avez moins de chance de réussite que si vous prenez une attaque moins puissante.
         tout ceci vous seras décris dans un tableau en dessus des instruction
       </p>
       <p>
-        Pour la phases de défense même principe, vous avez le choix entre trois type de défense différente,(nom des defenses) qui permette soit d'esquiver complètement l'attaque, sois d'affaiblir les dommage de celle ci
+        Pour la phases de défense même principe, vous avez le choix entre deux type de défense différente, qui permette soit d'esquiver complètement l'attaque, sois d'affaiblir les dommage de celle ci
         sachant que comme l'attaque si vous choisissez l'esquive complète vous avez moins de chance de réussite donc vous pouvez vous loupez et prendre l'attaque de plein fouet
         comme le reste dans le tableau si dessous vous verrez entièrement les caractéristique de chaque défense
       </p>
