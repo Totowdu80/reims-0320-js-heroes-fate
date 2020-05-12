2ec3f6eabefa48c24512f3acc0596b63167752b6
Totowdu80 <totowdu80@gmail.com>
2020-05-12 18:58:28 +0200

    refactor: add different type of defense

--- src/components/Homepage/Board/Game/Game.jsx
+++ src/components/Homepage/Board/Game/Game.jsx
@@ -17,7 +17,9 @@ class Game extends Component {
       playerLife: parseFloat(this.props.heroes[1].powerstats.power) * 2,
     };
     this.attackturn = this.attackturn.bind(this);
-    this.defenseturn = this.defenseturn.bind(this);
+    this.lowDef = this.lowDef.bind(this);
+    this.mediumDef = this.mediumDef.bind(this);
+    this.highDef = this.highDef.bind(this);
   }
 
   transition = () => {
@@ -119,70 +121,262 @@ class Game extends Component {
     }
   }
 
-  defenseturn() {
+  lowDef() {
     const enemyAttack = parseFloat(this.props.heroes[0].powerstats.strength);
     const playerDefense = (parseFloat(this.props.heroes[1].powerstats.durability) + parseFloat(this.props.heroes[1].powerstats.combat)) / 2;
     const dicePlayer = Math.floor(Math.random() * (Math.floor(100)));
     const diceIA = Math.floor(Math.random() * (Math.floor(100)));
     let attack = true;
-    let critical = true;
+    let attCritical = true;
     let defense = true;
+    let defCritical = true;
 
     if (diceIA <= 5) {
       attack = false;
-      critical = true;
+      attCritical = true;
     } else if (diceIA >= 6 && diceIA <= 49) {
       attack = false;
-      critical = false;
+      attCritical = false;
     } else if (diceIA >= 50 && diceIA <= 94) {
       attack = true;
-      critical = false;
+      attCritical = false;
     } else {
       attack = true;
-      critical = true;
+      attCritical = true;
     }
 
-    if (dicePlayer <= 50) {
+    if (dicePlayer <= 5) {
       defense = false;
+      defCritical = true;
+    } else if (dicePlayer >= 6 && dicePlayer <= 19) {
+      attack = false;
+      defCritical = false;
+    } else if (dicePlayer >= 20 && dicePlayer <= 94) {
+      attack = true;
+      defCritical = false;
     } else {
-      defense = true;
+      attack = true;
+      defCritical = true;
     }
 
-    if (attack === true && critical === true && defense === false) {
-      this.setState({
-        playerLife: this.state.playerLife - (enemyAttack * 2),
-        number: diceIA,
-        numberTwo: dicePlayer,
-        interface: this.state.interface + 1,
-      });
-    } else if (attack === true && critical === false && defense === false) {
-      this.setState({
-        playerLife: this.state.playerLife - (enemyAttack),
-        number: diceIA,
-        numberTwo: dicePlayer,
-        interface: this.state.interface + 1,
-      });
-    } else if (attack === true && critical === true && defense === true) {
-      this.setState({
-        playerLife: (this.state.playerLife + playerDefense) - (enemyAttack * 2),
-        number: dicePlayer,
-        numberTwo: diceIA,
-        interface: this.state.interface + 1,
-      });
-    } else if (attack === true && critical === false && defense === true) {
-      this.setState({
-        playerLife: (this.state.playerLife + playerDefense) - (enemyAttack),
-        number: dicePlayer,
-        numberTwo: diceIA,
-        interface: this.state.interface + 1,
-      });
-    } else {
-      this.setState({
-        number: dicePlayer,
-        numberTwo: diceIA,
-        interface: this.state.interface + 1,
-      });
+    let damage = 0;
+    let attLaunch = 'Attaque ratée';
+    let defLaunch = 'Défense ratée';
+
+    if (attack === true) {
+      if (attCritical === true) {
+        if (defense === true) {
+          if (defCritical === true) { /* Attaque Critique mais Défense Critique, reste bonus à appliquer */
+            damage = playerDefense - (enemyAttack * 2);
+            attLaunch = 'Attaque Critique';
+            defLaunch = 'Défense Critique';
+          } else { /* Attaque Critique, Défense Normale */
+            damage = playerDefense - (enemyAttack * 2);
+            attLaunch = 'Attaque Critique';
+            defLaunch = 'Défense réussie';
+          }
+        } else { /* Attaque Critque, aucune défense */
+          damage = -(enemyAttack * 2);
+          attLaunch = 'Attaque Critique';
+          defLaunch = 'Défense Ratée';
+        }
+      } else if (defense === true) {
+        if (defCritical === true) { /* Attaque non critique, Défense critique (bonus de def à rajouter) */
+          damage = playerDefense - enemyAttack;
+          attLaunch = 'Attaque réussie';
+          defLaunch = 'Défense Critique';
+        } else { /* Attaque non critique, Défense non critique */
+          damage = playerDefense - enemyAttack;
+          attLaunch = 'Attaque réussie';
+          defLaunch = 'Défense réussie';
+        }
+      } else { /* Attaque non critique, Pas de défense */
+        damage = -enemyAttack;
+        attLaunch = 'Attaque réussie';
+        defLaunch = 'Défense ratée';
+      }
     }
+
+    this.setState({
+      playerLife: this.state.playerLife + Math.min(damage, 0),
+      number: dicePlayer,
+      numberTwo: diceIA,
+      interface: this.state.interface + 1,
+      playerLaunch: defLaunch,
+      iaLaunch: attLaunch,
+    });
+  }
+
+  mediumDef() {
+    const enemyAttack = parseFloat(this.props.heroes[0].powerstats.strength);
+    const playerDefense = (parseFloat(this.props.heroes[1].powerstats.durability) + parseFloat(this.props.heroes[1].powerstats.combat)) / 2;
+    const dicePlayer = Math.floor(Math.random() * (Math.floor(100)));
+    const diceIA = Math.floor(Math.random() * (Math.floor(100)));
+    let attack = true;
+    let attCritical = true;
+    let defense = true;
+    let defCritical = true;
+
+    if (diceIA <= 5) {
+      attack = false;
+      attCritical = true;
+    } else if (diceIA >= 6 && diceIA <= 49) {
+      attack = false;
+      attCritical = false;
+    } else if (diceIA >= 50 && diceIA <= 94) {
+      attack = true;
+      attCritical = false;
+    } else {
+      attack = true;
+      attCritical = true;
+    }
+
+    if (dicePlayer <= 5) {
+      defense = false;
+      defCritical = true;
+    } else if (dicePlayer >= 6 && dicePlayer <= 49) {
+      attack = false;
+      defCritical = false;
+    } else if (dicePlayer >= 50 && dicePlayer <= 94) {
+      attack = true;
+      defCritical = false;
+    } else {
+      attack = true;
+      defCritical = true;
+    }
+
+    let damage = 0;
+    let attLaunch = 'Attaque ratée';
+    let defLaunch = 'Défense ratée';
+
+    if (attack === true) {
+      if (attCritical === true) {
+        if (defense === true) {
+          if (defCritical === true) { /* Attaque Critique mais Défense Critique, reste bonus à appliquer */
+            damage = playerDefense - (enemyAttack * 2);
+            attLaunch = 'Attaque Critique';
+            defLaunch = 'Défense Critique';
+          } else { /* Attaque Critique, Défense Normale */
+            damage = playerDefense - (enemyAttack * 2);
+            attLaunch = 'Attaque Critique';
+            defLaunch = 'Défense réussie';
+          }
+        } else { /* Attaque Critque, aucune défense */
+          damage = -(enemyAttack * 2);
+          attLaunch = 'Attaque Critique';
+          defLaunch = 'Défense Ratée';
+        }
+      } else if (defense === true) {
+        if (defCritical === true) { /* Attaque non critique, Défense critique (bonus de def à rajouter) */
+          damage = playerDefense - enemyAttack;
+          attLaunch = 'Attaque réussie';
+          defLaunch = 'Défense Critique';
+        } else { /* Attaque non critique, Défense non critique */
+          damage = playerDefense - enemyAttack;
+          attLaunch = 'Attaque réussie';
+          defLaunch = 'Défense réussie';
+        }
+      } else { /* Attaque non critique, Pas de défense */
+        damage = -enemyAttack;
+        attLaunch = 'Attaque réussie';
+        defLaunch = 'Défense ratée';
+      }
+    }
+
+    this.setState({
+      playerLife: this.state.playerLife + Math.min(damage, 0),
+      number: dicePlayer,
+      numberTwo: diceIA,
+      interface: this.state.interface + 1,
+      playerLaunch: defLaunch,
+      iaLaunch: attLaunch,
+    });
+  }
+
+  highDef() {
+    const enemyAttack = parseFloat(this.props.heroes[0].powerstats.strength);
+    const playerDefense = (parseFloat(this.props.heroes[1].powerstats.durability) + parseFloat(this.props.heroes[1].powerstats.combat)) / 2;
+    const dicePlayer = Math.floor(Math.random() * (Math.floor(100)));
+    const diceIA = Math.floor(Math.random() * (Math.floor(100)));
+    let attack = true;
+    let attCritical = true;
+    let defense = true;
+    let defCritical = true;
+
+    if (diceIA <= 5) {
+      attack = false;
+      attCritical = true;
+    } else if (diceIA >= 6 && diceIA <= 49) {
+      attack = false;
+      attCritical = false;
+    } else if (diceIA >= 50 && diceIA <= 94) {
+      attack = true;
+      attCritical = false;
+    } else {
+      attack = true;
+      attCritical = true;
+    }
+
+    if (dicePlayer <= 5) {
+      defense = false;
+      defCritical = true;
+    } else if (dicePlayer >= 6 && dicePlayer <= 79) {
+      attack = false;
+      defCritical = false;
+    } else if (dicePlayer >= 80 && dicePlayer <= 94) {
+      attack = true;
+      defCritical = false;
+    } else {
+      attack = true;
+      defCritical = true;
+    }
+
+    let damage = 0;
+    let attLaunch = 'Attaque ratée';
+    let defLaunch = 'Défense ratée';
+
+    if (attack === true) {
+      if (attCritical === true) {
+        if (defense === true) {
+          if (defCritical === true) { /* Attaque Critique mais Défense Critique, reste bonus à appliquer */
+            damage = playerDefense - (enemyAttack * 2);
+            attLaunch = 'Attaque Critique';
+            defLaunch = 'Défense Critique';
+          } else { /* Attaque Critique, Défense Normale */
+            damage = playerDefense - (enemyAttack * 2);
+            attLaunch = 'Attaque Critique';
+            defLaunch = 'Défense réussie';
+          }
+        } else { /* Attaque Critque, aucune défense */
+          damage = -(enemyAttack * 2);
+          attLaunch = 'Attaque Critique';
+          defLaunch = 'Défense Ratée';
+        }
+      } else if (defense === true) {
+        if (defCritical === true) { /* Attaque non critique, Défense critique (bonus de def à rajouter) */
+          damage = playerDefense - enemyAttack;
+          attLaunch = 'Attaque réussie';
+          defLaunch = 'Défense Critique';
+        } else { /* Attaque non critique, Défense non critique */
+          damage = playerDefense - enemyAttack;
+          attLaunch = 'Attaque réussie';
+          defLaunch = 'Défense réussie';
+        }
+      } else { /* Attaque non critique, Pas de défense */
+        damage = -enemyAttack;
+        attLaunch = 'Attaque réussie';
+        defLaunch = 'Défense ratée';
+      }
+    }
+
+    this.setState({
+      playerLife: this.state.playerLife + Math.min(damage, 0),
+      number: dicePlayer,
+      numberTwo: diceIA,
+      interface: this.state.interface + 1,
+      playerLaunch: defLaunch,
+      iaLaunch: attLaunch,
+    });
   }
 
   render() {
@@ -196,8 +390,9 @@ class Game extends Component {
           {this.state.interface === 0
             && (
             <div>
-              <p>Your Turn</p>
-              <input type="button" className="styled" onClick={this.transition} value="Continue" />
+              <p className="text__slideleft">Your</p>
+              <p className="text__slideright"> Turn</p>
+              <input type="button" className="continueButton" onClick={this.transition} value="Continue" />
             </div>
             )}
 
@@ -205,28 +400,28 @@ class Game extends Component {
             && (
             <div>
               {/* Attack Choice */}
-              <input type="button" className="styled" onClick={this.attackturn} value="Attack 1" />
-              <input type="button" className="styled" onClick={this.transition} value="Attack 2" />
-              <input type="button" className="styled" onClick={this.transition} value="Attack 3" />
+              <input type="button" className="gameButton" onClick={this.attackturn} value="Attack 1" />
+              <input type="button" className="gameButton" onClick={this.transition} value="Attack 2" />
+              <input type="button" className="gameButton" onClick={this.transition} value="Attack 3" />
             </div>
             )}
 
           {this.state.interface === 2
             && (
-            <div>
+            <div className="result__container">
               {/* Résultats */}
-              <div>
-                <p>{this.state.iaLaunch}</p>
-                <p>{this.state.playerLaunch}</p>
+              <div className="textresult__container">
+                <p className="textresult">{this.state.iaLaunch}</p>
+                <p className="textresult">{this.state.playerLaunch}</p>
               </div>
-              <div>
+              <div className="dice__container">
                 <button className="dice" type="button">{this.state.numberTwo}</button>
                 <button className="dice" type="button">{this.state.number}</button>
               </div>
               <div>
                 {this.state.enemyLife <= 0
-                  ? <input type="button" className="styled" onClick={this.endWin} value="Finish" />
-                  : <input type="button" className="styled" onClick={this.transition} value="Continue" />}
+                  ? <input type="button" className="gameButton" onClick={this.endWin} value="Finish" />
+                  : <input type="button" className="continueButton" onClick={this.transition} value="Continue" />}
               </div>
             </div>
             )}
@@ -235,7 +430,7 @@ class Game extends Component {
             && (
             <div>
               <p>Enemy Turn</p>
-              <input type="button" className="styled" onClick={this.transition} value="Continue" />
+              <input type="button" className="continueButton" onClick={this.transition} value="Continue" />
             </div>
             )}
 
@@ -243,28 +438,28 @@ class Game extends Component {
             && (
             <div>
               {/* Défense Choice */}
-              <input type="button" className="styled" onClick={this.defenseturn} value="Defense 1" />
-              <input type="button" className="styled" onClick={this.transition} value="Defense 2" />
-              <input type="button" className="styled" onClick={this.transition} value="Defense 3" />
+              <input type="button" className="gameButton" onClick={this.lowDef} value="Defense 1" />
+              <input type="button" className="gameButton" onClick={this.mediumDef} value="Defense 2" />
+              <input type="button" className="gameButton" onClick={this.highDef} value="Defense 3" />
             </div>
             )}
 
           {this.state.interface === 5
             && (
-            <div>
+            <div className="result__container">
               {/* Résultats */}
-              <div>
-                <p>{this.state.iaLaunch}</p>
-                <p>{this.state.playerLaunch}</p>
+              <div className="textresult__container">
+                <p className="textresult">{this.state.iaLaunch}</p>
+                <p className="textresult">{this.state.playerLaunch}</p>
               </div>
-              <div>
+              <div className="dice__container">
                 <button className="dice" type="button">{this.state.numberTwo}</button>
                 <button className="dice" type="button">{this.state.number}</button>
               </div>
               <div>
                 {this.state.playerLife <= 0
-                  ? <input type="button" className="styled" onClick={this.endLose} value="Finish" />
-                  : <input type="button" className="styled" onClick={this.restart} value="Continue" />}
+                  ? <input type="button" className="gameButton" onClick={this.endLose} value="Finish" />
+                  : <input type="button" className="continueButton" onClick={this.restart} value="Continue" />}
               </div>
             </div>
             )}
@@ -273,7 +468,7 @@ class Game extends Component {
             && (
             <div>
               <p>You Win</p>
-              <input type="button" className="styled" onClick={this.restart} value="Continue" />
+              <input type="button" className="continueButton" onClick={this.restart} value="Continue" />
             </div>
             )}
 
@@ -281,7 +476,7 @@ class Game extends Component {
             && (
             <div>
               <p>You Lose</p>
-              <input type="button" className="styled" onClick={this.restart} value="Continue" />
+              <input type="button" className="continueButton" onClick={this.restart} value="Continue" />
             </div>
             )}
         </div>
