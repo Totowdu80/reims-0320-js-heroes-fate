b8f1fa190cc505765e322b094a0531f2f638bae2
Totowdu80 <totowdu80@gmail.com>
2020-05-15 12:35:32 +0200

    refactor: final adjustment on the app

--- src/components/Homepage/Instructions/instruction.jsx
+++ src/components/Homepage/Instructions/instruction.jsx
@@ -25,11 +25,11 @@ class Instruction extends React.Component {
               En haut de ce dernier, vous pouvez apercevoir la partie d'interface concernant votre ennemi : son nom, son avatar, ses points de vie ainsi que ses différentes statistiques
             </p>
             <p>
-              Au centre de votre écran se trouve le plateau de jeu, dans lequel toutes les actions principales vont se dérouler et dans lequel vont s'enchainer des tours endiablés de combat
-              entre votre héro et son ennemi.
+              Au centre de votre écran se trouve le plateau de jeu, dans lequel toutes les actions principales vont se dérouler et dans lequel vont s'enchainer des tours endiablés de combats
+              entre votre héros et son ennemi.
             </p>
             <p>
-              Enfin, au bas de votre écran se trouve, la partie d'interface de votre héro qui, tout comme votre ennemi, présentera son nom, son avatar, sa vie ainsi que ses stats
+              Enfin, au bas de votre écran se trouve, la partie d'interface de votre héros qui, tout comme votre ennemi, présentera son nom, son avatar, sa vie ainsi que ses statistiques.
             </p>
           </div>
 
@@ -49,19 +49,19 @@ class Instruction extends React.Component {
               Les combats se découpent en 2 phases par tour : une première phase qui sera l'attaque et la deuxième qui sera la défense.
             </p>
             <p>
-              La stats d'initiative déterminera qui du joueur ou de l'ennemi attaquera le premier : celui avec la stats la plus haute sera le premier à attaquer
+              La statistique d'initiative déterminera qui du joueur ou de l'ennemi attaquera le premier : celui avec la statistique la plus haute sera le premier à attaquer
             </p>
             <div className="instrucPart1">
               <img className="imgPlateau" src="https://zupimages.net/up/20/20/zz0t.png" alt="choix d'attaque" />
               <div className="parAttak">
                 <p>
-                  Une fois cela déterminé, le combat peut commencer, le premier joueur entre alors en phase d'attaque et accède ainsi à une interface lui proposant 3 attaques : attaque faible,
-                  attaque normale et attaque puissante, chacune ayant leurs propres caratéristiques. Plus une attaque fera de dégâts, moins elle aura de chance de réussir.
+                  Une fois cela déterminé, le combat peut commencer, le premier joueur entre alors en phase d'attaque et accède ainsi à une interface lui proposant 3 attaques : petite attaque,
+                  attaque moyenne et puissante attaque, chacune ayant leurs propres caratéristiques. Plus une attaque fera de dégâts, moins elle aura de chance de réussir.
                 </p>
                 <p>
                   Une fois l'attaque choisie par le joueur, un dé est alors lancé permettant de calculer la réussite de celle ci et c'est alors au joueur adverse de choisir sa défense, définit sur
-                  3 choix différents tout comme l'attaque avec également un taux de réussite dépend des dégâts bloqués. Tout comme l'attaque, une fois le choix fait, un dé est alors lancé afin de définir
-                  si oui ou non la défense est réussie ou pas.
+                  3 choix différents tout comme l'attaque avec également un taux de réussite qui dépend des dégâts bloqués. Tout comme l'attaque, une fois le choix fait, un dé est alors lancé afin de définir
+                  si oui ou non la défense est réussie.
                 </p>
               </div>
             </div>
@@ -69,7 +69,7 @@ class Instruction extends React.Component {
             <div className="instrucPart1">
               <div className="parDee">
                 <p>
-                  Une fois ces choix fait par chacun des joueurs et les dés lancés, le combat commence alors et si réussite il y a alors l'attaquant infligera des dégats au défenseur dépendant de sa réussite
+                  Une fois ces choix fait par chacun des joueurs et les dés lancés, le combat commence alors et, si réussite il y a, alors l'attaquant infligera des dégats au défenseur dépendant de sa réussite
                   également.
                 </p>
 
@@ -85,13 +85,13 @@ class Instruction extends React.Component {
 
         <div className="buttonPlayInstruc">
           <div>
-            <p className="outro">Tout cela semble clair pour vous ? Alors lancez vous et allez ré-écrire l'histoire de votre héro préféré !</p>
+            <p className="outro">Tout cela semble clair pour vous ? Alors lancez-vous et allez ré-écrire l'histoire de votre héros préféré !</p>
           </div>
           <button
             className="homeButton"
             onClick={this.props.clickUniverse}
             type="button">
-            <span>Play</span>
+            Jouer
           </button>
         </div>
 
